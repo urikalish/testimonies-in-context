1201990b2c54cc5dde38438cee2327d850eb2bb3
Uri Kalish <uri.kalish@hpe.com>
2016-06-09 19:08:27 +0300

    post via angular

--- tic-client/js/tic-comm-serv.js
+++ tic-client/js/tic-comm-serv.js
@@ -1,11 +1,14 @@
-angular.module('ticApp').factory('ticCommServ', function ticCommServ(ticConstants, ticVariables) {
+angular.module('ticApp').factory('ticCommServ', function ticCommServ($http, ticConstants, ticVariables) {
 
   function sendServerRequest(from, to, callback) {
-    $.post(ticConstants.url.apiBase, {from: from, to: to}, function(response) {
-      var resObj = JSON.parse(response);
-      var markersData = resObj['markersData'];
-      var eventsData = resObj['eventsData'];
-      callback(markersData, eventsData);
+    var req = {
+      method: 'POST',
+      url: ticConstants.url.apiBase,
+      headers: {'Content-Type': 'application/json'},
+      data: {from: from, to: to}
+    };
+    $http(req).then(function(response) {
+      callback(response.data.markersData, response.data.eventsData);
     });
   }
 
