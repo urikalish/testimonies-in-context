5ff13549b8e06923d9d4d48a506997dea3e0565a
Uri Kalish <uri.kalish@hpe.com>
2016-06-11 21:41:04 +0300

    unsupported browser message

--- tic-client/js/tic-init.js
+++ tic-client/js/tic-init.js
@@ -1,10 +1,8 @@
 function initTic() {
   if (navigator.userAgent.toLowerCase().indexOf('chrome') > -1) {
+    var errMsgCtrl = document.getElementById('browser-not-supported');
+    errMsgCtrl.style.display = 'none';
     document.getElementById('tic-container').style.display = 'block';
     setTimeout(function() { angular.element(document.body).injector().get('ticServ').init(); }, 1000);
-  } else {
-    var errMsgCtrl = document.getElementById('error-message');
-    errMsgCtrl.innerHTML = 'Your browser type is not yet supported, please use Chrome.';
-    errMsgCtrl.style.display = 'inline';
   }
 }
