17c5d047ac166c8a577864f0787667fad9a16d34
Uri Kalish <uri.kalish@hpe.com>
2016-06-12 02:49:37 +0300

    info window

--- tic-client/js/tic-map-serv.js
+++ tic-client/js/tic-map-serv.js
@@ -12,6 +12,43 @@ angular.module('ticApp').factory('ticMapServ', function ticMapServ(ticConstants,
     );
   }
 
+  window.showLocationInStreetView = function showLoationInStreetView(lat, lon) {
+    var fenway = {'lat': lat, 'lng': lon};
+    var panorama = new google.maps.StreetViewPanorama(
+      $('#' + ticConstants.ctrlId.GOOGLE_MAP)[0], {
+        position: fenway,
+        pov: {
+          heading: 34,
+          pitch: 10
+        }
+      });
+    window.googleMapObj.setStreetView(panorama);
+  };
+
+  window.getInfoWindowContentHtml = function getInfoWindowHtml(data) {
+    var infoWindowContentHtml = '';
+    infoWindowContentHtml +=  '<div class="info-window-container">';
+    infoWindowContentHtml +=      '<div class="info-window-top">';
+    infoWindowContentHtml +=          '<img src="img/bookmark.png" class="info-window-bookmark"/>';
+    infoWindowContentHtml +=          '<img src="img/eye.png" class="info-window-eye" onclick="showLocationInStreetView(50.037141, 19.180081)"/>';
+    infoWindowContentHtml +=      '</div>';
+    infoWindowContentHtml +=      '<div class="info-window-content">';
+    infoWindowContentHtml +=          '<img src="img/left.png" class="info-window-left"/>';
+    if (data.type === 'video') {
+      infoWindowContentHtml +=        '<video class="info-window-video" controls autoplay><source src="' + data.url + '" type="video/mp4"/></video>';
+    } else if (data.type === 'audio') {
+      infoWindowContentHtml +=        '<audio class="info-window-audio" controls><source src="' + data.url + '" type="audio/ogg"/></audio>';
+    } else if (data.url) {
+      infoWindowContentHtml +=        '<img class="info-window-image" src="' + data.url + '"/>';
+    }
+    infoWindowContentHtml +=          '<img src="img/right.png" class="info-window-right"/>';
+    infoWindowContentHtml +=      '</div>';
+    infoWindowContentHtml +=      '<div class="info-window-text">' + data.text + '</div>';
+    infoWindowContentHtml +=      '<div class="info-window-related"><a href="">Related items</a></div>';
+    infoWindowContentHtml +=  '</div>';
+    return infoWindowContentHtml;
+  };
+
   function removeAllMarkers() {
     _.forEach(ticVariables.markers, function(m) {
       m.setMap(null);
@@ -33,45 +70,21 @@ angular.module('ticApp').factory('ticMapServ', function ticMapServ(ticConstants,
     });
   }
 
-  window.showLoationInStreetView = function showLoationInStreetView(lat, lon) {
-    var fenway = {'lat': lat, 'lng': lon};
-    var panorama = new google.maps.StreetViewPanorama(
-      $('#' + ticConstants.ctrlId.GOOGLE_MAP)[0], {
-        position: fenway,
-        pov: {
-          heading: 34,
-          pitch: 10
-        }
-      });
-    window.googleMapObj.setStreetView(panorama);
-  };
-
   function addMarker(data) {
-    var marker, contentString, infowindow;
-    var topbarHtmlTemplate = '<div><img src="img/bookmark.png" class="info-window-bookmark"/><img src="img/eye.png" class="info-window-eye" onclick="showLoationInStreetView(50.037141, 19.180081)"/></div>';
-    var textHtmlTemplate = '<div class="info-window-text">' + data.text + '</div>';
-    var relatedItemsHtmlTemplate = '<div class="info-window-related"><a href="">Related items</a></div>';
-    if (data.type === 'video') {
-      contentString = '<div style="margin-top:20px">' + topbarHtmlTemplate + '<div class="info-window-content"><img src="img/left.png" class="info-window-left"/>' + '<video class="info-window-video" controls autoplay><source src="' + data.url + '" type="video/mp4"/></video>' + '<img src="img/right.png" class="info-window-right"/></div>' + textHtmlTemplate + relatedItemsHtmlTemplate + '</div>';
-    } else if (data.type === 'audio') {
-      contentString = '<div style="margin-top:20px">' + topbarHtmlTemplate + '<div class="info-window-content"><img src="img/left.png" class="info-window-left"/>' + '<audio class="info-window-audio" controls><source src="' + data.url + '" type="audio/ogg"/></audio>' + '<img src="img/right.png" class="info-window-right"/></div>' + textHtmlTemplate + relatedItemsHtmlTemplate + '</div>';
-    } else {
-      contentString = '<div style="margin-top:20px">' + topbarHtmlTemplate + '<div class="info-window-content"><img src="img/left.png" class="info-window-left"/>' + (data.url ? '<img class="info-window-image" src="' + data.url + '"/>' : '') + '<img src="img/right.png" class="info-window-right"/></div>' + textHtmlTemplate + relatedItemsHtmlTemplate + '</div>';
-    }
-    marker = new google.maps.Marker({
+    var marker = new google.maps.Marker({
       map: window.googleMapObj,
       position: {lat: data.lat, lng: data.lng},
       icon: ticConstants.url.imgBase + '/' + data.type + '.png'
-    });
-    infowindow = new google.maps.InfoWindow({
-      content: contentString,
-      maxWidth: 400
     });
     marker.addListener('click', function() {
       if (data.type === 'document') {
         window.open(data.url);
       } else {
-        infowindow.open(window.googleMapObj, marker);
+        var infoWindow = new google.maps.InfoWindow({
+          content: getInfoWindowContentHtml(data),
+          maxWidth: 400
+        });
+        infoWindow.open(window.googleMapObj, marker);
       }
     });
     return marker;
