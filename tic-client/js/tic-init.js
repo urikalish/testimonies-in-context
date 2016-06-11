4d79f2b2945f9250e4b221908af0da79f289c2f1
Uri Kalish <uri.kalish@hpe.com>
2016-06-12 02:59:45 +0300

    loading message

--- tic-client/js/tic-init.js
+++ tic-client/js/tic-init.js
@@ -1,8 +1,10 @@
 function initTic() {
-  if (navigator.userAgent.toLowerCase().indexOf('chrome') > -1) {
-    var errMsgCtrl = document.getElementById('browser-not-supported');
-    errMsgCtrl.style.display = 'none';
-    document.getElementById('tic-container').style.display = 'block';
-    setTimeout(function() { angular.element(document.body).injector().get('ticServ').init(); }, 1000);
-  }
+  setTimeout(function() {
+    if (navigator.userAgent.toLowerCase().indexOf('chrome') > -1) {
+      var errMsgCtrl = document.getElementById('browser-not-supported');
+      errMsgCtrl.style.display = 'none';
+      document.getElementById('tic-container').style.display = 'block';
+      angular.element(document.body).injector().get('ticServ').init();
+    }
+  }, 1000);
 }
